aa669a989709cdc80fd1292b7af84d134859bde6
ryota <mc_ryouta3@yahoo.co.jp>
2024-12-05 19:08:40 +0900

    [fix] 最終ステータス画面のもろもろのバグ解消

--- routes/status.py
+++ routes/status.py
@@ -24,19 +24,17 @@ def add():
     if request.method == 'POST':
         user_id = request.form['user_id']
         item_id = request.form['item_id']
+
         # 選択されたユーザーと装備を取得
         selected_user = User.get(User.id == user_id)
         selected_item = Item.get(Item.id == item_id)
-        
-        # ユーザーの名前と装備の名前を計算または表示
-        total_name = selected_user.name
-        total_item = selected_item.name
+
         # ユーザーのHPと装備のHPを計算または表示
         total_hp = int(selected_user.hp + selected_item.hitpoint)
         total_at = int(selected_user.at + selected_item.attack)
         total_df = int(selected_user.df + selected_item.defence)
 
-        Status.create(name=total_name, item=total_item, hp=total_hp, at=total_at, df=total_df)
+        Status.create(name=selected_user, item=selected_item, hp=total_hp, at=total_at, df=total_df)
         return redirect(url_for('status.list'))
     
     users = User.select()
@@ -45,35 +43,22 @@ def add():
 
 @status_bp.route('/edit/<int:status_id>', methods=['GET', 'POST'])
 def edit(status_id):
-    status = Status.get_or_none(Status.id == status_id)
+    status = Status.get(Status.id == status_id)
     if not status:
         return redirect(url_for('status.list'))
 
-    total_user = ""
-    total_item = ""
-    total_hp = 0
-    total_at = 0
-    total_df = 0
     if request.method == 'POST':
-        status.name = request.form['user_id']
-        status.item = request.form['item_id']
-
-        # 選択されたユーザーと装備を取得
-        selected_user = User.get(User.id == status.name)
-        selected_item = Item.get(Item.id == status.item)
-        
-        # ユーザーの名前と装備の名前を計算または表示
-        total_user = selected_user.name
-        total_item = selected_item.name
-        # ユーザーのHPと装備のHPを計算または表示
-        total_hp = int(selected_user.hp + selected_item.hitpoint)
-        total_at = int(selected_user.at + selected_item.attack)
-        total_df = int(selected_user.df + selected_item.defence)
-
+        input_name = request.form['user_id']
+        input_item = request.form['item_id']
+        status.name = User.get(User.id == input_name)
+        status.item = Item.get(Item.id == input_item)
+        status.hp = int(status.name.hp + status.item.hitpoint)
+        status.at = int(status.name.at + status.item.attack)
+        status.df = int(status.name.df + status.item.defence)
         status.save()
         return redirect(url_for('status.list'))
 
-    users = total_user
-    items = total_item 
+    users = User.select()
+    items = Item.select()
 
-    return render_template('status_edit.html',status=status, users=users, items=items)
+    return render_template('status_edit.html', status=status, users=users, items=items)
