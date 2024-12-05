5fa080e38182509c7dedc120989f7704f5802a9e
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-12-05 16:23:17 +0900

    error:スコアの追加ができない

--- models/score.py
+++ models/score.py
@@ -1,4 +1,4 @@
-from peewee import Model, ForeignKeyField, DateTimeField
+from peewee import Model, ForeignKeyField, IntegerField
 from .db import db
 from .challenger import Challenger
 from .song import Song
@@ -6,7 +6,7 @@ from .song import Song
 class Score(Model):
     challenger = ForeignKeyField(Challenger, backref='scores')
     song = ForeignKeyField(Song, backref='scores')
-    score = DateTimeField()
+    score = IntegerField()
 
     class Meta:
         database = db
