a61ce99a87029f5c3371d1bc46d365a30cb176ff
K2277 <k22077kk@aitech.ac.jp>
2024-12-05 16:21:50 +0900

    weapon.pyの修正

--- models/weapon.py
+++
@@ -1,9 +0,0 @@
-from peewee import Model, CharField, DecimalField
-from .db import db
-
-class Weapon(Model):
-    name = CharField()
-    price = DecimalField()
-
-    class Meta:
-        database = db
