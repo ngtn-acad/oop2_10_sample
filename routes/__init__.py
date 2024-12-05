176fbef939eed82ae654707db69a50a685790579
satooru65536 <ryuto4619@gmail.com>
2024-12-05 14:55:41 +0900

    style: routes を変更

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,6 @@
 from .user import user_bp
-# from .product import product_bp
-# from .order import order_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
     user_bp,
-    # product_bp,
-    # order_bp
 ]
