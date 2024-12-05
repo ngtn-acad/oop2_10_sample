6df56fdb7902774a830509267ea1fa99aeab4b12
hiroto09 <h16100929@icloud.com>
2024-12-05 15:47:58 +0900

    feat: ホーム周りの修正

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,10 @@
 from .user import user_bp
-from .product import product_bp
-from .order import order_bp
+from .item import item_bp
+from .status import status_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
-  product_bp,
-  order_bp
+  item_bp,
+  status_bp
 ]
