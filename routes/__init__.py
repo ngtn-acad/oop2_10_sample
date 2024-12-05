28a5507f65fb5bc82fd82b460723a279540a5c39
satooru65536 <ryuto4619@gmail.com>
2024-12-05 14:54:02 +0900

    feat: ユーザ登録画面

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,10 @@
 from .user import user_bp
-from .product import product_bp
-from .order import order_bp
+# from .product import product_bp
+# from .order import order_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
-  user_bp,
-  product_bp,
-  order_bp
+    user_bp,
+    # product_bp,
+    # order_bp
 ]
--- routes/user.py
+++ routes/user.py
@@ -2,40 +2,44 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from models import User
 
 # Blueprintの作成
-user_bp = Blueprint('user', __name__, url_prefix='/users')
+user_bp = Blueprint("user", __name__, url_prefix="/users")
 
 
-@user_bp.route('/')
+@user_bp.route("/")
 def list():
-    
+
     # データ取得
     users = User.select()
 
-    return render_template('user_list.html', title='ユーザー一覧', items=users)
+    return render_template("user_list.html", title="ユーザー一覧", items=users)
 
 
-@user_bp.route('/add', methods=['GET', 'POST'])
+@user_bp.route("/add", methods=["GET", "POST"])
 def add():
-    
-    if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        User.create(name=name, age=age)
-        return redirect(url_for('user.list'))
-    
-    return render_template('user_add.html')
+
+    if request.method == "POST":
+        student_id = request.form["student_id"]
+        name = request.form["name"]
+        gender = request.form["gender"]
+        age = request.form["age"]
+
+        User.create(student_id=student_id, name=name, gender=gender, age=age)
+
+        return redirect(url_for("user.list"))
+
+    return render_template("user_add.html")
 
 
-@user_bp.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+@user_bp.route("/edit/<int:user_id>", methods=["GET", "POST"])
 def edit(user_id):
     user = User.get_or_none(User.id == user_id)
     if not user:
-        return redirect(url_for('user.list'))
+        return redirect(url_for("user.list"))
 
-    if request.method == 'POST':
-        user.name = request.form['name']
-        user.age = request.form['age']
+    if request.method == "POST":
+        user.name = request.form["name"]
+        user.age = request.form["age"]
         user.save()
-        return redirect(url_for('user.list'))
+        return redirect(url_for("user.list"))
 
-    return render_template('user_edit.html', user=user)
+    return render_template("user_edit.html", user=user)
