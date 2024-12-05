76a02dc64552d7e0ca0011eba1d2ffecba1c7941
nanamori <ohayoru@gmail.com>
2024-12-05 15:41:17 +0900

    add student_info

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,12 @@
 from .user import user_bp
 from .product import product_bp
 from .order import order_bp
+from .student_info import student_info_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
   product_bp,
-  order_bp
+  order_bp,
+  student_info_bp,
 ]
