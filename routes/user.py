f54b4e3f824d65e7f3ee0c91121e02a334ac9c48
ayu <umaminokiami@gmail.com>
2024-12-05 14:59:31 +0900

    fix-user.py

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,54 @@
-from peewee import Model, CharField, IntegerField
-from .db import db
+from flask import Blueprint, render_template, request, redirect, url_for
+from models import User
 
-class User(Model):
-    name = CharField()
-    age = IntegerField()
+# Blueprintの作成
+user_bp = Blueprint('user', __name__, url_prefix='/users')
 
-    class Meta:
-        database = db
+
+@user_bp.route('/')
+def list():
+    
+    # データ取得
+    users = User.select()
+
+    return render_template('user_list.html', title='ユーザー一覧', items=users)
+
+
+@user_bp.route('/add', methods=['GET', 'POST'])
+def add():
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        User.create(name=name, age=age)
+        return redirect(url_for('user.list'))
+    
+    return render_template('user_add.html')
+
+
+@user_bp.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit(user_id):
+    user = User.get_or_none(User.id == user_id)
+    if not user:
+        return redirect(url_for('user.list'))
+
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.age = request.form['age']
+        user.save()
+        return redirect(url_for('user.list'))
+
+    return render_template('user_edit.html', user=user)
+
+@user_bp.route('/delete/<int:user_id>', methods=['GET', 'POST'])
+def delete(user_id):
+    user = User.get_or_none(User.id == user_id)
+    if not user:
+        return redirect(url_for('user.list'))
+
+    if request.method == 'POST':
+        # データを削除
+        user.delete_instance()
+        return redirect(url_for('user.list'))
+
+    return render_template('user_delete.html', user=user)
