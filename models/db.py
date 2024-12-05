8f2f8381271f7e97f77c14c16c2ae41444c4ec09
yuuki-cloud <119059243+yuuki-cloud@users.noreply.github.com>
2024-12-05 14:30:13 +0900

    データベースの初期化

--- models/db.py
+++ models/db.py
@@ -1,4 +1,33 @@
-from peewee import SqliteDatabase
+from peewee import *
 
 # データベース接続の定義
 db = SqliteDatabase('my_database.db')
+
+class BaseModel(Model):
+    """A base model that will use our Sqlite database."""
+    class Meta:
+        database = db
+
+class User_tb(BaseModel):
+    user_id = IntegerField()
+    username = CharField()
+    age = IntegerField()
+   
+class Task_tb(BaseModel):
+    user_id = IntegerField()
+    task_name = CharField()
+    task_content = CharField()
+
+class Physical_tb(BaseModel):
+    user_id = IntegerField()
+    temp = DoubleField()
+    bad_good = BooleanField(default=True)
+    bad_content = CharField()
+
+# データベース接続とテーブル作成
+db.connect()
+db.create_tables([User_tb])
+db.create_tables([Task_tb])
+db.create_tables([Physical_tb])
+
+db.close()
