f765eddcacde7aa37b21b01a90e1ca76c1277a44
hiroto09 <h16100929@icloud.com>
2024-12-05 18:03:49 +0900

    feat:総合ステータスのaddの修正

--- routes/status.py
+++ routes/status.py
@@ -16,23 +16,27 @@ def list():
 
 @status_bp.route('/add', methods=['GET', 'POST'])
 def add():
+    total_name = ""
+    total_item = ""
     total_hp = 0
     total_at = 0
     total_df = 0
     if request.method == 'POST':
         user_id = request.form['user_id']
         item_id = request.form['item_id']
-
         # 選択されたユーザーと装備を取得
         selected_user = User.get(User.id == user_id)
         selected_item = Item.get(Item.id == item_id)
         
+        # ユーザーの名前と装備の名前を計算または表示
+        total_name = selected_user.name
+        total_item = selected_item.name
         # ユーザーのHPと装備のHPを計算または表示
         total_hp = int(selected_user.hp + selected_item.hitpoint)
         total_at = int(selected_user.at + selected_item.attack)
         total_df = int(selected_user.df + selected_item.defence)
 
-        Status.create(name=user_id, item=item_id, hp=total_hp, at=total_at, df=total_df)
+        Status.create(name=total_name, item=total_item, hp=total_hp, at=total_at, df=total_df)
         return redirect(url_for('status.list'))
     
     users = User.select()
@@ -45,6 +49,8 @@ def edit(status_id):
     if not status:
         return redirect(url_for('status.list'))
 
+    total_user = ""
+    total_item = ""
     total_hp = 0
     total_at = 0
     total_df = 0
@@ -56,6 +62,9 @@ def edit(status_id):
         selected_user = User.get(User.id == status.name)
         selected_item = Item.get(Item.id == status.item)
         
+        # ユーザーの名前と装備の名前を計算または表示
+        total_user = selected_user.name
+        total_item = selected_item.name
         # ユーザーのHPと装備のHPを計算または表示
         total_hp = int(selected_user.hp + selected_item.hitpoint)
         total_at = int(selected_user.at + selected_item.attack)
@@ -64,7 +73,7 @@ def edit(status_id):
         status.save()
         return redirect(url_for('status.list'))
 
-    users = User.select()
-    items = Item.select()
+    users = total_user
+    items = total_item 
 
     return render_template('status_edit.html', users=users, items=items, total_hp=total_hp, total_at=total_at, total_df=total_df)
