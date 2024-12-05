c3fabd3a017e4f0db681d40ae48fd05718c315aa
k23096 <k23096kk@aitech.ac.jp>
2024-12-05 14:42:25 +0900

    goods

--- routes/goods.py
+++ routes/goods.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from models import Product
 
 # Blueprintの作成
-product_bp = Blueprint('product', __name__, url_prefix='/products')
+product_bp = Blueprint('goods', __name__, url_prefix='/goods')
 
 
 @product_bp.route('/')
 def list():
     products = Product.select()
-    return render_template('product_list.html', title='商品一覧', items=products)
+    return render_template('goods_list.html', title='商品一覧', items=products)
 
 
 @product_bp.route('/add', methods=['GET', 'POST'])
@@ -19,21 +19,21 @@ def add():
         name = request.form['name']
         price = request.form['price']
         Product.create(name=name, price=price)
-        return redirect(url_for('product.list'))
+        return redirect(url_for('goods.list'))
     
-    return render_template('product_add.html')
+    return render_template('goods_add.html')
 
 
-@product_bp.route('/edit/<int:product_id>', methods=['GET', 'POST'])
-def edit(product_id):
-    product = Product.get_or_none(Product.id == product_id)
+@product_bp.route('/edit/<int:goods_id>', methods=['GET', 'POST'])
+def edit(goods_id):
+    product = Product.get_or_none(Product.id == goods_id)
     if not product:
-        return redirect(url_for('product.list'))
+        return redirect(url_for('goods.list'))
 
     if request.method == 'POST':
         product.name = request.form['name']
         product.price = request.form['price']
         product.save()
-        return redirect(url_for('product.list'))
+        return redirect(url_for('goods.list'))
 
-    return render_template('product_edit.html', product=product)
+    return render_template('goods_edit.html', goods=product)
