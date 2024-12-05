f78890539e7d8c6d1d561f3a71c1bbda2bc7be5f
k23052kk <k23052kk@aitech.ac.jp>
2024-12-05 15:12:49 +0900

    k23052変更かけました

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
-from peewee import Model, CharField, IntegerField
+from peewee import Model, CharField
 from .db import db
 
 class User(Model):
     name = CharField()
-    age = IntegerField()
+    gender = CharField()
 
     class Meta:
         database = db
