7a5f17893980bf501733b3e4403f550bca8e0be3
k23002 <k23002kk@aitech.ac.jp>
2024-12-05 15:45:18 +0900

    Chenge product.py

--- routes/product.py
+++ routes/product.py
@@ -8,7 +8,8 @@ product_bp = Blueprint('product', __name__, url_prefix='/products')
 @product_bp.route('/')
 def list():
     products = Product.select()
-    return render_template('product_list.html', title='製品一覧', items=products)
+    print(products)
+    return render_template('product_list.html', title='動物一覧', items=products)
 
 
 @product_bp.route('/add', methods=['GET', 'POST'])
@@ -16,9 +17,11 @@ def add():
     
     # POSTで送られてきたデータは登録
     if request.method == 'POST':
+        kind = request.form['kind']
         name = request.form['name']
-        price = request.form['price']
-        Product.create(name=name, price=price)
+        food = request.form['food']
+
+        Product.create(kind=kind, name=name, food=food)
         return redirect(url_for('product.list'))
     
     return render_template('product_add.html')
@@ -31,8 +34,9 @@ def edit(product_id):
         return redirect(url_for('product.list'))
 
     if request.method == 'POST':
+        product.kind = request.form['kind']
         product.name = request.form['name']
-        product.price = request.form['price']
+        product.food = request.form['food']
         product.save()
         return redirect(url_for('product.list'))
 
