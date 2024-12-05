4b74b5b43334e91c881ec7989cf5b78b9df4749d
tomiyuki03 <youxifuze41@gmail.com>
2024-12-05 15:52:09 +0900

    変更

--- routes/challenger.py
+++ routes/challenger.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from models import User
+from models import Challenger
 
 # Blueprintの作成
 challenger_bp = Blueprint('challenger', __name__, url_prefix='/challengers')
@@ -9,18 +9,19 @@ challenger_bp = Blueprint('challenger', __name__, url_prefix='/challengers')
 def list():
     
     # データ取得
-    users = Challenger.select()
+    challengers = Challenger.select()
 
-    return render_template('challenger_list.html', title='チャンレジャー一覧', items=users)
+    return render_template('challenger_list.html', title='チャンレジャー一覧', items=challengers)
 
 
+@challenger_bp.route('/add', methods=['GET', 'POST'])
 def add():
     
     if request.method == 'POST':
         name = request.form['name']
         age = request.form['age']
         gender = request.form['gender']
-        User.create(name=name, age=age,gender=gender)
+        Challenger.create(name=name, age=age,gender=gender)
         return redirect(url_for('challenger.list'))
     
     return render_template('challenger_add.html')
@@ -30,12 +31,12 @@ def add():
 def edit(challenger_id):
     challenger = Challenger.get_or_none(Challenger.id == challenger_id)
     if not challenger:
-        return redirect(url_for('user.list'))
+        return redirect(url_for('challenger.list'))
 
     if request.method == 'POST':
         challenger.name = request.form['name']
         challenger.age = request.form['age']
         challenger.save()
-        return redirect(url_for('user.list'))
+        return redirect(url_for('challenger.list'))
 
     return render_template('challenger_edit.html', challenger=challenger)
