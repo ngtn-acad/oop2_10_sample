1632f75b8484187cc3b5bc04c8ce9058ead467df
k23056 <k23056kk@aitech.ac.jp>
2024-12-05 15:18:38 +0900

    Update

--- routes/user.py
+++ routes/user.py
@@ -39,3 +39,13 @@ def edit(user_id):
         return redirect(url_for('user.list'))
 
     return render_template('user_edit.html', user=user)
+
+@user_bp.route('search_name', methods=['GET', 'POST'])
+def search():
+    search_name = request.args.get('search_name', '') 
+    users = []
+
+    if search_name:
+        users = User.select().where(User.name == search_name)
+
+    return render_template('user_search.html', title='ユーザー名検索', items=users, search_name=search_name)
