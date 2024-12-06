05f6e6ef44fbc21a6dbf7fb8ad3457d28e292979
KT128-end <137659812+KT128-end@users.noreply.github.com>
2024-12-06 15:29:01 +0900

    modelsの２つとroutesのBlueprintをリストを追加しました
    ####機能は確認してません

--- models/employee.py
+++ models/employee.py
@@ -1,9 +1,11 @@
 from peewee import *
 from .db import db
+from models import Hire
 
 # モデル定義
 class Employee(Model):
     employee_id = AutoField()  # 主キー（自動生成される一意の識別子）
+    hire_id = ForeignKeyField(Hire,backref="employees")
     first_name = CharField(max_length=10)  # 名（最大10文字）
     last_name = CharField(max_length=10)  # 姓（最大10文字）
     gender = CharField(max_length=5)  # 性別（最大5文字）
@@ -12,6 +14,9 @@ class Employee(Model):
     phone_number = CharField(max_length=20, unique=True)  # 電話番号（一意）
     address = TextField()  # 住所
 
+    @property
+    def hire(self):
+        return self.hire_id
+
     class Meta:
         database = db  # 使用するデータベースを指定
-
