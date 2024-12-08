1a0e35c8acc75d91c5e3e145f5554e5ef13f6249
k23232-ait <k23232kk@aitech.ac.jp>
2024-12-09 04:44:45 +0900

    Add: Quest, Fix: Weapon(DB)
    Questの追加
    あとWeapon.pyのEdit時の修正

--- routes/quest.py
+++ routes/quest.py
@@ -1,43 +1,47 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from models import Quest, Character, Weapon
+from models import  Character, Weapon, Quest
 from datetime import datetime
 
 # Blueprintの作成
-quest_bp = Blueprint('quest', __name__, url_prefix='/orders')
+quest_bp = Blueprint('quest', __name__, url_prefix='/quests')
 
 
 @quest_bp.route('/')
 def list():
-    orders = Quest.select()
-    return render_template('order_list.html', title='注文一覧', items=orders)
+    quests = Quest.select()
+    return render_template('order_list.html', title='クエスト一覧', items=quests)
 
 
 @quest_bp.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        user_id = request.form['user_id']
-        product_id = request.form['product_id']
-        order_date = datetime.now()
-        Quest.create(user=user_id, product=product_id, order_date=order_date)
-        return redirect(url_for('order.list'))
+        name = request.form['name']
+        character_id = request.form['character_id']
+        weapon_id = request.form['weapon_id']
+        quest_date = datetime.now()
+        Quest.create(name=name, character=character_id, weapon=weapon_id, quest_date=quest_date)
+        return redirect(url_for('quest.list'))
     
     characters = Character.select()
     weapons = Weapon.select()
-    return render_template('order_add.html', users=characters, products=weapons)
+    return render_template('order_add.html', characters=characters, weapons=weapons)
 
 
-@quest_bp.route('/edit/<int:order_id>', methods=['GET', 'POST'])
-def edit(order_id):
-    quest = Quest.get_or_none(Quest.id == order_id)
+@quest_bp.route('/edit/<int:quest_id>', methods=['GET', 'POST'])
+def edit(quest_id):
+    quest = Quest.get_or_none(Quest.id == quest_id)
     if not quest:
-        return redirect(url_for('order.list'))
+        return redirect(url_for('quest.list'))
 
     if request.method == 'POST':
-        quest.user = request.form['user_id']
-        quest.product = request.form['product_id']
+
+        quest.name = request.form['name']
+        quest.character = request.form['character_id']
+        quest.weapon = request.form['weapon_id']
         quest.save()
-        return redirect(url_for('order.list'))
+        
+        return redirect(url_for('quest.list'))
 
     characters = Character.select()
     weapons = Weapon.select()
-    return render_template('order_edit.html', order=quest, users=characters, products=weapons)
+    return render_template('order_edit.html', quest=quest, characters=characters, weapons=weapons)
