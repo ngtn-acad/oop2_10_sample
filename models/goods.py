5ce457cf81b4e051181bfb437d1c5ce3cba66c32
k23096 <k23096kk@aitech.ac.jp>
2024-12-10 15:42:04 +0900

    Update goods

--- models/goods.py
+++ models/goods.py
@@ -1,11 +1,9 @@
 from peewee import Model, CharField, DecimalField
 from .db import db
 
-
 class Goods(Model):
     name = CharField()
     price = DecimalField()
-    categori = DecimalField()
-
+    categori=DecimalField()
     class Meta:
         database = db
