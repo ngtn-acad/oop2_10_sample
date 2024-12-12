e01e488ed591e9860775a33387eb9556bdac9d66
hiroto09 <h16100929@icloud.com>
2024-12-12 14:54:05 +0900

    add:グラフのテンプレートの作成とHeaderの作成

--- app.py
+++ app.py
@@ -14,7 +14,17 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # ダミーデータ
+    data = {
+        "labels": ["9:00", "10:00", "11:00", "12:00", "13:00"],  # 時間
+        "datasets": [
+            {
+                "label": "Temperature (°C)",
+                "data": [20, 22, 24, 23, 21],  # 温度
+            }
+        ]
+    }
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(debug=True)
