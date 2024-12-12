defd55c4ed3ad4fafb6dcfa5b707008dedb0a297
Sakai <135956101+Sakai1123@users.noreply.github.com>
2024-12-12 15:53:00 +0900

    人気な店舗ランキング

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from models import Food
+from peewee import fn
 
 app = Flask(__name__)
 
@@ -14,7 +16,9 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    restaurant_ranking = Food.select(Food.restaurant,fn.Avg(Food.evaluation)).group_by(Food.restaurant).order_by(fn.Avg(Food.evaluation).desc()).limit(5)
+
+    return render_template('index.html',restaurant_ranking=restaurant_ranking)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
